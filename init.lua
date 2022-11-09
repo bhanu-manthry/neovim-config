cdb03c21d71db5eabed4f7ba85db02e134b5efd2
Bhanu Manthry <bhanuprasad.manthry@pentair.com>
2022-11-09 07:09:49 +0530

    first commit

---
+++ init.lua
@@ -0,0 +1,9 @@
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.colorscheme"
+require "user.nvim-tree"
+require "user.bufferline"
+require "user.toggleterm"
+require "user.treesitter"
+
